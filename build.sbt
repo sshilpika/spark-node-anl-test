ec2681be3d5713795d9664b58d6f839e3d672b2a
Shilpika S <sshilpika5@gmail.com>
2016-06-15 10:30:44 -0500

    DF and RDD sort

--- build.sbt
+++ build.sbt
@@ -8,9 +8,12 @@ resolvers += "Typesafe repository releases" at "http://repo.typesafe.com/typesaf
 
 libraryDependencies ++= Seq(
   "org.scalatest" %% "scalatest" % "2.2.6" % Test,
-  "org.apache.spark" % "spark-core_2.10" % "1.6.1" % "provided"
+  "org.apache.spark" % "spark-core_2.10" % "1.6.1" % "provided",
+  "org.apache.spark" % "spark-sql_2.10" % "1.6.1" % "provided",
+  "com.github.marklister" %% "product-collections" % "1.4.3",
+  "com.github.scopt" %% "scopt" % "3.4.0"
 )
 
-mainClass in assembly := Some("fileG")
+mainClass in assembly := Some("gov.anl.alcf.fileG")
 
 //sources in Compile <<= (sources in Compile).map(_ filter(_.name == "fileG.scala"))
--- src/main/scala/fileG.scala
+++ src/main/scala/fileG.scala
@@ -1,27 +1,103 @@
-import org.apache.spark.rdd.RDD
+package gov.anl.alcf
+
+import java.io.{File, FileOutputStream, PrintWriter}
+
 import util.Random
+import org.apache.spark.rdd.RDD
 import org.apache.spark._
+import org.apache.spark.sql._
+
+case class Config(fileSize: Option[Int] = Some(10))
+
 object fileG{
-    val conf = new SparkConf().setAppName("Random Integer Sort File Generator")
-    val spark = new SparkContext(conf)
+  val conf = new SparkConf().setAppName("Random Integer Sort File Generator")
+  val spark = new SparkContext(conf)
+
+  val sqlContext = new org.apache.spark.sql.SQLContext(spark)
+  import sqlContext.implicits._
 
 	def main(args:Array[String]):Unit = {
 
-      val mustSort = fileGen
-      println(RddSort(mustSort))
+    // getting the values for ingestion using scopt
+    val appConfig = parseCommandLine(args).getOrElse(Config())
+    val size = appConfig.fileSize.getOrElse(10)
+
+    val mustSort = fileGen(size).persist()
+
+    println("RDD SORTED ")
+      val RDDSort= performance{
+        RddSort(mustSort)
+      }
+
+
+    val mustSortDF = mustSort.toDF()
+    println("DF sorted :")
+    val DFSort = performance {
+      dataFrameSort(mustSortDF)
+    }
+
+    //TODO write results to CSV instead of text file
+    //val RDDres = List(("time",RDDSort._1),("space",RDDSort._2),("result_length",RDDSort._3)).csvIterator.mkString("\n")
+
+    //write to results file
+    val writer = new PrintWriter(new FileOutputStream(new File("Results.txt"), true))
+    writer.append(s"RDDTime : ${RDDSort._1}, RDDSpace : ${RDDSort._2}, RDDResult: ${RDDSort._3}\n"++
+      s"DFTime : ${DFSort._1}, DFSpace : ${DFSort._2}, DFResult: ${DFSort._3}\n\n"
+    )
+    writer.close()
+
+
+    stopSpark
+
+  }
+
+  def fileGen(size : Int): RDD[Int] ={
+    val one = spark.parallelize(Seq.fill(size)(size))
+    one.flatMap(x => Seq.fill(x)(Random.nextInt))
+  }
+
+  def fileGenMakeRDD(size : Int): RDD[Int] ={
+    val one = spark.makeRDD(Seq.fill(size)(size))
+    one.flatMap(x => Seq.fill(x)(Random.nextInt))
+  }
+
+    def RddSort(mustSort: RDD[Int]): Long ={
+      val result = mustSort.sortBy(c=>c,true)
+      result.saveAsTextFile("RDDSortedRes")
+      result.count()
+
+    }
+
+    def dataFrameSort(df: DataFrame): Long ={
+
+      val sorted = df.sort("_1")
+      val writeInto = sorted.write.save("DFSortedRes")
+
+      sorted.count()
 
 
   }
-    def fileGen(): RDD[Int] ={
-        val one = spark.parallelize(Seq.fill(10)(10))
-        one.flatMap(x => Seq.fill(x)(Random.nextInt))
-    }
+  /*def dataSetSort(ds: Dataset): Array[Int] ={
 
-    def RddSort(mustSort: RDD[Int]): Array[Int] ={
-        val result = mustSort.sortBy(c=>c,true)
-        result.saveAsTextFile("RDDSortedRes")
-        result.take(100)
+    ds.
+    //ds.s //sort().write.save("df.txt")
+    ds.take(100).asInstanceOf[Array[Int]]
 
+  }*/
+
+  def parseCommandLine(args: Array[String]): Option[Config] = {
+    val parser = new scopt.OptionParser[Config]("scopt") {
+      head("Ingest", "1.0")
+      opt[Int]('s', "size") action { (x, c) =>
+        c.copy(fileSize = Some(x))
+      } text ("fileSize is a Int property")
+      help("help") text ("Usage: -s [fileSize]")
     }
+    parser.parse(args, Config())
+  }
+
+  def stopSpark(): Unit ={
+    spark.stop()
+  }
 
 }
